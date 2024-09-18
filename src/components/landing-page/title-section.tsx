f0ddb95fea700832ddbd3bd07f0800606b3f4631
kingstondoesit <kingstondoesitall@gmail.com>
2024-09-19 00:30:14 +0100

    modify title-section.tsx

--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -14,12 +14,24 @@ export const TitleSection: React.FC<TitleSectionProps> = ({
   return (
     <React.Fragment>
       <section className='flex flex-col gap-4 justify-center items-start md:items-center'>
-        <article className='rounded-full p-[1px] dark:bg-gradient-to-r dark:from-brand-primaryBlue dark:to-brand-primaryPurple'>
-            <div className= 'rounded-full px-3 py-1 dark:bg-black'> {pill} </div>
+        <article className='rounded-full p-[1px] dark:bg-gradient-to-l dark:from-brand-primaryBlue dark:to-brand-primaryPurple'>
+          <div className='rounded-full px-3 py-1 dark:bg-black'> {pill} </div>
         </article>
-        {subheading? (<><h2 className='text-left text-3xl sm:text-5xl sm:max-w-[750px] md:text-center font-semibold'>{title}</h2>
-        <p className='dark:text-washed-purple-700 sm:max-w-[450px]'></p></> ):(<h1 className='text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text-center font-semibold'>{title}</h1>)}
-          </section>
+        {subheading ? (
+          <>
+            <h2 className='dark:text-brand-white text-left text-3xl sm:text-5xl sm:max-w-[750px] md:text-center font-semibold'>
+              {title}
+            </h2>
+            <p className='dark:text-washed-purple-400 sm:max-w-[450px] md:text-center'>
+              {subheading}
+            </p>
+          </>
+        ) : (
+          <h1 className='dark:text-brand-white text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text-center font-semibold'>
+            {title}
+          </h1>
+        )}
+      </section>
     </React.Fragment>
   );
 };
