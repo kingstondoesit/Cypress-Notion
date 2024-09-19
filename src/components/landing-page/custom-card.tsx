ff7991a65beff8e7d389e01acefa46f240ce37be
kingstondoesit <kingstondoesitall@gmail.com>
2024-09-20 00:58:41 +0100

    feat: add shadcn ui card

---
+++ src/components/landing-page/custom-card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
+import { cn } from '../../lib/utils';
+
+
+
+const CustomCard = () => {
+  <div>
+      CustomCard
+    </div>
+  ;
+};
+
+export default CustomCard;
