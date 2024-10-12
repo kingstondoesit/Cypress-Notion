03448ee59fd80937cef4aedafa47f41f5cb38ab3
kingstondoesit <kingstondoesitall@gmail.com>
2024-10-12 23:15:20 +0100

    Add custom card and avatars | fix broken import paths

--- src/components/landing-page/custom-card.tsx
+++ src/components/landing-page/custom-card.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
 import { cn } from '../../lib/utils';
 
-
-
-const CustomCard = () => {
-  <div>
-      CustomCard
-    </div>
-  ;
+type CardProps = React.ComponentProps<typeof Card>;
+type CustomCardProps = CardProps & {
+  cardHeader?: React.ReactNode;
+  cardFooter?: React.ReactNode;
+  cardContent?: React.ReactNode;
 };
+
+const CustomCard: React.FC<CustomCardProps> = ({
+  className,
+  cardHeader,
+  cardFooter,
+  cardContent,
+  ...props
+}) => {
+  return (
+    <Card
+    className={cn('w-[300px]', className)}
+      {...props}
+    >
+      <CardHeader>
+        {cardHeader}
+      </CardHeader>
+      <CardContent className='grid gap-4'>
+        {cardContent}
+      </CardContent>
+      <CardFooter>
+        {cardFooter}
+      </CardFooter>
+    </Card>
+  )};
 
 export default CustomCard;
